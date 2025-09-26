480ea1e5f00422ba0f7cc8f143a4540dd7615913
fothgarrick <fothgarrick@gmail.com>
2025-09-25 19:46:46 -0600

    Finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+
+        Console.WriteLine("Welcome to the Guessing Game!");
+        
+            while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct!");
+            }
+        }
+        }
 }
+
+
+
+// string answer = Console.ReadLine();
+//    int percent = int.Parse(answer);
